f4da69cc3b8773f7c36b923b8f1557cfc4ece1b8
Marcin Bulanda <themarcin349@gmail.com>
2024-03-01 10:37:11 +0100

    Gameboard funtionality added.

--- Puzzle_JMMB/MainPage.xaml.cs
+++ Puzzle_JMMB/MainPage.xaml.cs
@@ -14,5 +14,57 @@ namespace Puzzle_JMMB
         {
             InitializeComponent();
         }
+
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            int id = int.Parse(btn.AutomationId.Substring(3, 1));
+            Button newBtn;
+            if(btn.Text != "")
+            {
+                try
+                {
+                    newBtn = (Button)FindByName("Btn" + (id + 3).ToString());
+                    if(newBtn.Text == "")
+                    {
+                        newBtn.Text = btn.Text;
+                        btn.Text = "";
+                    }
+                }
+                catch { }
+                try
+                {
+                    newBtn = (Button)FindByName("Btn" + (id - 3).ToString());
+                    if (newBtn.Text == "")
+                    {
+                        newBtn.Text = btn.Text;
+                        btn.Text = "";
+                    }
+                }
+                catch { }
+                try
+                {
+                    newBtn = (Button)FindByName("Btn" + (id + 1).ToString());
+                    if (newBtn.Text == "")
+                    {
+                        newBtn.Text = btn.Text;
+                        btn.Text = "";
+                    }
+                }
+                catch { }
+                try
+                {
+                    newBtn = (Button)FindByName("Btn" + (id - 1).ToString());
+                    if (newBtn.Text == "")
+                    {
+                        newBtn.Text = btn.Text;
+                        btn.Text = "";
+                    }
+                }
+                catch { }
+            }
+
+            
+        }
     }
 }
