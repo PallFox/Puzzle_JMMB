118ff4c38137cca460648e21a5c21bc1914e2cb4
Jakub Mitkowski <graczkuba2005@gmail.com>
2024-03-01 10:37:27 +0100

    Winning check function added.

--- Puzzle_JMMB/MainPage.xaml.cs
+++ Puzzle_JMMB/MainPage.xaml.cs
@@ -10,9 +10,19 @@ namespace Puzzle_JMMB
 {
     public partial class MainPage : ContentPage
     {
+        Random rand;
+
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void WinCheck()
+        {
+            if(Btn1.Text == "1" && Btn2.Text == "2" && Btn3.Text == "3" && Btn4.Text == "4" && Btn5.Text == "5" && Btn6.Text == "6" && Btn7.Text == "7" && Btn8.Text == "8")
+            {
+                DisplayAlert("Puzzle", "Wygrałeś!", "OK");
+            }
         }
     }
 }
